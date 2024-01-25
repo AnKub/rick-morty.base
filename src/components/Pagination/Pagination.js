7eaa329a4c259aadb88617803fe1106600a4763b
AnKub <ankub3088@gmail.com>
2024-01-25 22:49:42 +0200

    done

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -36,19 +36,19 @@ const Pagination = ({ pageNumber, info, updatePageNumber }) => {
         `}
       </style>
       <ReactPaginate
-        className="pagination justify-content-center my-4 gap-4"
+        className=" pagination justify-content-center my-4 gap-4"
         nextLabel="Next"
         forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
         previousLabel="Prev"
-        previousClassName="btn btn-primary fs-5 prev"
-        nextClassName="btn btn-primary fs-5 next"
-        activeClassName="active"
-        marginPagesDisplayed={width < 576 ? 1 : 2}
-        pageRangeDisplayed={width < 576 ? 1 : 2}
-        pageCount={info?.pages}
-        onPageChange={pageChange}
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
+        previousClassName=" btn btn-primary fs-5 prev "
+        nextClassName=" btn btn-primary fs-5 next "
+        activeClassName=" active "
+        marginPagesDisplayed={ width < 576 ? 1 : 2 }
+        pageRangeDisplayed={ width < 576 ? 1 : 2 }
+        pageCount={ info?.pages }
+        onPageChange={ pageChange }
+        pageClassName=" page-item"
+        pageLinkClassName=" page-link"
       />
     </>
   );
