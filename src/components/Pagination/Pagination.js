6d8eac3bb9775918d4a500ab7db972867f67df25
AnKub <ankub3088@gmail.com>
2024-01-25 21:54:49 +0200

    done

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,22 +1,57 @@
-import React from 'react';
-import ReactPaginate from 'react-paginate';
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 
-const Pagination = ({info, pageNumber, setPageNumber}) => {
-
-  return <ReactPaginate 
-  className='pagination justify-content-center gap-4 my-4'
-  forcePage={pageNumber===1? 0 : pageNumber-1 }
-  nextLabel='Next'
-  previousLabel='Prev'
-  nextclassNameName = ' btn btn-primary '
-  previousclassNameName = ' btn btn-primary '
-  pageclassNameName=' page-item '
-  pageLinkclassNameName= ' page-link '
-  activeclassNameName= ' active '
-  onPageChange={(data) => {
-  setPageNumber(data.selected+1);
-  }}
-  pageCount={info?.pages}/>;
+const Pagination = ({ pageNumber, info, updatePageNumber }) => {
+  let pageChange = (data) => {
+    updatePageNumber(data.selected + 1);
   };
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  return (
+    <>
+      <style jsx>
+        {`
+          @media (max-width: 768px) {
+            .pagination {
+              font-size: 12px;
+            }
+            .next,
+            .prev {
+              display: none;
+            }
+          }
+          @media (max-width: 768px) {
+            .pagination {
+              font-size: 14px;
+            }
+          }
+        `}
+      </style>
+      <ReactPaginate
+        className="pagination justify-content-center my-4 gap-4"
+        nextLabel="Next"
+        forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+        previousLabel="Prev"
+        previousClassName="btn btn-primary fs-5 prev"
+        nextClassName="btn btn-primary fs-5 next"
+        activeClassName="active"
+        marginPagesDisplayed={width < 576 ? 1 : 2}
+        pageRangeDisplayed={width < 576 ? 1 : 2}
+        pageCount={info?.pages}
+        onPageChange={pageChange}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+      />
+    </>
+  );
+};
 
 export default Pagination;
