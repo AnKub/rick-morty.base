29b737e741f36dc7770749efa04c0e70dabdd4d1
AnKub <ankub3088@gmail.com>
2024-01-26 12:16:27 +0200

    solvethebr

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
 
-const Pagination = ({ pageNumber, info, setPageNumber }) => {
-   const [width, setWidth] = useState(window.innerWidth);
+const Pagination = ({setPageNumber,  pageNumber, info }) => {
+  let pageChange = (data) => {setPageNumber(data.selected + 1)
+  };
+
+  const [width, setWidth] = useState(window.innerWidth);
   const updateDimensions = () => {
     setWidth(window.innerWidth);
   };
@@ -32,19 +35,19 @@ const Pagination = ({ pageNumber, info, setPageNumber }) => {
         `}
       </style>
       <ReactPaginate
-        className=" pagination justify-content-center my-4 gap-4"
+        className="pagination justify-content-center my-4 gap-4"
         nextLabel="Next"
         forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
         previousLabel="Prev"
-        previousClassName=" btn btn-primary fs-5 prev "
-        nextClassName=" btn btn-primary fs-5 next "
-        activeClassName=" active "
-        marginPagesDisplayed={ width < 576 ? 1 : 2 }
-        pageRangeDisplayed={ width < 576 ? 1 : 2 }
-        pageCount={ info?.pages }
-        onPageChange={ pageChange }
-        pageClassName=" page-item"
-        pageLinkClassName=" page-link"
+        previousClassName="btn btn-primary fs-5 prev"
+        nextClassName="btn btn-primary fs-5 next"
+        activeClassName="active"
+        marginPagesDisplayed={width < 576 ? 1 : 2}
+        pageRangeDisplayed={width < 576 ? 1 : 2}
+        pageCount={info?.pages}
+        onPageChange={pageChange}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
       />
     </>
   );
